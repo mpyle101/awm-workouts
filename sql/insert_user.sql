2d3ce5392c8fea0136adbad02325d65502be37b3
Michael Pyle <mpyle101@gmail.com>
2021-02-27 12:18:31 -0700

    Latest workouts and encrypted password

--- sql/insert_user.sql
+++ sql/insert_user.sql
@@ -1,7 +1,7 @@
 INSERT INTO awm.user (username, password, email, first_name, last_name)
 VALUES (
     ${username},
-    ${password},
+    crypt(${password}, gen_salt('bf', 8)),
     ${email},
     ${first_name},
     ${last_name}
