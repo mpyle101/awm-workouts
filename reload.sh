51cf42bb2632f66aa111b4bac8e4121e1acd64e7
Michael Pyle <mpyle101@gmail.com>
2022-12-07 17:44:59 -0700

    Skipping mongo

--- reload.sh
+++ reload.sh
@@ -1,9 +1,9 @@
-#! /opt/homebrew/bin/fish
+#! /bin/zsh
 
 osascript ./export.scpt
 python3 ./reader.py
 
-mongo awm --eval "db.dropDatabase()"
-mongoimport --db awm --collection cycles --file cycles.json --jsonArray
-mongoimport --db awm --collection exercises --file exercises.json --jsonArray
-mongoimport --db awm --collection workouts --file workouts.json --jsonArray
+#mongo awm --eval "db.dropDatabase()"
+#mongoimport --db awm --collection cycles --file cycles.json --jsonArray
+#mongoimport --db awm --collection exercises --file exercises.json --jsonArray
+#mongoimport --db awm --collection workouts --file workouts.json --jsonArray
--- setupdb.sh
+++ setupdb.sh
@@ -1,3 +1,7 @@
 #! /bin/zsh
 
 sqlite awm.db ".read ./sql/schema-sqlite.sql"
+
+#dropdb -h localhost -U jester awm
+#createdb -h localhost -U jester awm
+#psql -h localhost -U jester -d awm -f ./sql/schema.sql -q
